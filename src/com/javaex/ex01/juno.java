4a4f90cf27861020cf40da0d7d3d7c6430146679
wmswm <wmswm@211.226.214.35>
2022-04-13 17:04:50 +0900

    2022 04 13 오후 실습등등

---
+++ src/com/javaex/ex01/juno.java
@@ -0,0 +1,28 @@
+package com.javaex.ex01;
+
+public class juno {
+	public static void main(String[]args) {
+		
+		String name = "소준호";
+		System.out.println(name);
+		
+		String shcool = "상지대학교";
+		System.out.println(shcool);
+		
+		String major = "도시조경인테리어과";
+		System.out.println(major);
+		
+		String juminno = "981020 0000000";
+		System.out.println(juminno);
+		
+		int age = 25;
+		System.out.println(age);
+		
+		String phoneno = "010 8222 6682";
+		System.out.println(phoneno);
+		
+		String address = "인천광역시 미추홀구";
+		System.out.println(address);
+				
+	}	
+}
