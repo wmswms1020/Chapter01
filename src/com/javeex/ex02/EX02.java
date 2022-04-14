eb04a4019ab8e97d75da50100d8d864cceb63b46
wmswm <wmswm@211.226.214.35>
2022-04-14 18:18:26 +0900

    2022 04 14 8교시끝

---
+++ src/com/javeex/ex02/EX02.java
@@ -0,0 +1,19 @@
+package com.javeex.ex02;
+
+import java.util.Scanner;
+
+public class EX02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println("입력하신 숫자는 " +num+ "입니다.");
+		
+		sc.close();
+
+		
+		
+	}
+	
+}
---
+++ src/com/javeex/ex02/Ex01.java
@@ -0,0 +1,45 @@
+package com.javeex.ex02;
+
+public class Ex01 {
+
+	public static void main(String [] args) {
+		
+		int i = 2345;
+		double d = 3.14;
+		char c = '한';
+		String s = "한";
+		String str = "굿모닝";
+		String name = "소준호";
+		
+		System.out.println("안녕하세요");
+		System.out.println(str);
+		
+		
+		System.out.println(str + str);
+		System.out.println(str + i);
+		String var01 = str+i;
+		System.out.println(str + " 랑 " + i);
+		
+		System.out.println(str + d);
+		
+		
+		System.out.println(c);
+		System.out.println(c + c);
+		
+		System.out.println(s);
+		System.out.println(s + s);
+		
+		
+		System.out.println("제이름은 "+name+" 입니다.");
+		
+		System.out.println("--------------------------");
+		
+		
+		System.out.println("안녕\\하\\세여");
+		
+		
+		
+	} 
+	
+	
+}
